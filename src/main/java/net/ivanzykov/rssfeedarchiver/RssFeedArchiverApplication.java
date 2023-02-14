96323b4d1cf33c93013ac12f6fe2e12ede93d1fe
Ivan Zykov <ivan.v.zykov@gmail.com>
2023-02-14 16:44:27 +0100

    Adds initial Spring Boot files

---
+++ src/main/java/net/ivanzykov/rssfeedarchiver/RssFeedArchiverApplication.java
@@ -0,0 +1,13 @@
+package net.ivanzykov.rssfeedarchiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RssFeedArchiverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RssFeedArchiverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/ivanzykov/rssfeedarchiver/RssFeedArchiverApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ivanzykov.rssfeedarchiver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RssFeedArchiverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
